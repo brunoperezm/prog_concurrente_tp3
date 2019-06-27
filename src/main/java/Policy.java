9b82ca77bc84792d1db2e8df2bbbd442c8c7428b
jerebntz <jerebntz@gmail.com>
2019-06-27 10:28:16 -0300

    eye candy on Policy code

--- src/main/java/Policy.java
+++ src/main/java/Policy.java
@@ -3,7 +3,7 @@ class Policy {
 	public static TasksManager.CPUNumber getCpuBuffer(PN pn) {
 		int buffer1Tokens = pn.getPlaceTokens(PN.Places.Buffer1);
 		int buffer2Tokens = pn.getPlaceTokens(PN.Places.Buffer2);
-		if (buffer1Tokens > buffer2Tokens) return TasksManager.CPUNumber.CPU2;
-		else return TasksManager.CPUNumber.CPU1;
+
+		 return (buffer1Tokens > buffer2Tokens) ? TasksManager.CPUNumber.CPU2 : TasksManager.CPUNumber.CPU1;
 	}
 }
