b909f58850becc64f52ad6e8606dbc006b6163d8
jerebntz <jerebntz@gmail.com>
2019-06-27 14:48:09 -0300

    eye candy on TaskManager code

--- src/main/java/TasksManager.java
+++ src/main/java/TasksManager.java
@@ -16,18 +16,14 @@ class TasksManager extends Thread {
 	@Override
 	public void run() {
 	    // Prepare
-		PN.Transitions StartService;
-		PN.Transitions EndServiceRate;
+		PN.Transitions StartService = (mCPUNumber.equals(CPUNumber.CPU1))
+				? PN.Transitions.START_SERVICE_1
+				: PN.Transitions.START_SERVICE_2;
+		PN.Transitions EndServiceRate = (mCPUNumber.equals(CPUNumber.CPU1))
+				? PN.Transitions.END_SERVICE_RATE_1
+				: PN.Transitions.END_SERVICE_RATE_2;
 
-	    if(mCPUNumber.equals(CPUNumber.CPU1)){
-	    	StartService = PN.Transitions.START_SERVICE_1;
-			EndServiceRate = PN.Transitions.END_SERVICE_RATE_1;
-		}
-	    else {
-			StartService = PN.Transitions.START_SERVICE_2;
-			EndServiceRate = PN.Transitions.END_SERVICE_RATE_2;
-		}
-	    // Infinite Loop
+	    	    // Infinite Loop
 		while (!interrupted()) {
 				mMonitor.fireTransitions(Utils.getSingleTransition(StartService));
 				mMonitor.fireTransitions(Utils.getSingleTransition(EndServiceRate));
